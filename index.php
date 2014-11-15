f15ef75cd5246ad2bbffd23da37bbb5a8c1a3d03
jem <jemmiller805@gmail.com>
2014-11-15 02:08:38 -0600

    added php app

---
+++ index.php
@@ -0,0 +1,28 @@
+<?PHP
+
+if ($_GET['color'] == ''){
+  $_GET['color'] = 'blue';
+}
+
+?>
+
+
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<b><i><font color="<?PHP echo $_GET['color']; ?>"><font size=9><center>Ahoy mateys!!!</center></font></fo$
+<script src="script.js"></script>
+
+Hello
+<?php
+
+echo $_GET["name"];
+
+?>
+
+</body>
+</html>
