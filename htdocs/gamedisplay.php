87f8f1db6238c588cdbe8cf47cce33d3e0dc73c5
jem <jemmiller805@gmail.com>
2015-01-08 15:51:36 -0600

    added comment section

---
+++ htdocs/gamedisplay.php
@@ -0,0 +1,102 @@
+
+<?PHP
+
+
+require 'championlist.php';
+
+#print_r($champions);
+	
+
+require 'vendor/autoload.php';
+use LeagueWrap\Api;
+
+$api = new Api('3c89c9d9-6196-463b-9c7b-2ef2f83540f8');
+
+$summonerAPI = $api->summoner();
+$gameAPI = $api->game();
+
+$name = $_POST['name'];
+
+$summoner = $summonerAPI->info($name);
+
+$recentGames = $gameAPI->recent($summoner->id);
+
+echo "<img src='http://avatar.leagueoflegends.com/na/". ($name) . ".png' >" ;
+echo "<br>". ($name) ."'s Past 10 Games: <br>";
+
+function fixlolnum($lolnumber) {
+	if ($lolnumber == ''){
+	  return 0;
+	} else {
+	  return $lolnumber;
+	}
+}
+
+
+function fixlolname($lolname) {
+	$lolname = str_replace(" ", "", $lolname);
+	$lolname = str_replace("'", "", $lolname);
+	$lolname = str_replace(".", "", $lolname);
+	return $lolname;
+}
+
+?>
+<table>
+<tr>
+	<td></td>
+	<td>Stats</td>
+	<td></td>
+</tr>
+<?PHP
+
+foreach ($recentGames->games as $gameNum => $game) {
+	$championId = $game->championId;
+	$championName = $champions[$championId];	
+	echo "<!---#$championId--->\n";
+
+	echo "<tr>";
+	echo "<td>";
+	echo "<img src='/images/" . fixlolname($championName) . "Square.png'>";	
+	echo "<br>";
+	echo $championName;
+	echo "</td>";
+
+	echo "<td>";
+	echo fixlolnum($game->stats->championsKilled);
+	echo "/";
+	echo fixlolnum($game->stats->numDeaths);
+	echo "/";
+	echo fixlolnum($game->stats->assists);
+	echo "</td>";
+	
+
+	echo "<td>";
+	$won = $game->stats->win;
+
+	if ($won == 1){
+	  echo "<span class='big victory'>Victory</span>";
+	}
+	else {
+	  echo "<span class='big defeat'>Defeat</span>";
+
+	}
+	echo "<br>";
+	if (fixlolnum($game->stats->numDeaths) > fixlolnum($game->stats->championsKilled)+4) {
+	  echo " ...Goddamn feeder.";
+	}
+	if ((fixlolnum($game->stats->championsKilled) > fixlolnum($game->stats->numDeaths)+4) && $won == 1) {
+	  echo " ...Good job!";
+	}	
+
+	echo "</td>";
+	echo "</tr>\n\n";
+}
+#foreach ($champions as $championId => $championName) {
+#	
+#	echo "<img src='/images/" . fixlolname($championName) . "Square.png'>";	
+#}
+
+#print_r($recentGames->games[0]->stats);
+?>
+</table>
+<br><br><br>
--- htdocs/index.php
+++ htdocs/index.php
@@ -80,117 +80,38 @@ Hello
 <?php
 if (!isset($_POST["name"])){
 	echo "Summoner";
-	exit();
+	echo "<br><br>";
+	
 } else {
 	echo $_POST["name"];
+	echo "<br><br><br>";
+	require 'gamedisplay.php';
 }
 
+function sanitize($input) {
+	$output = mysqli_real_escape_string($connection, $input);
+	return $output;
+}
 
-?>
-<br><Br><br>
-
-<?PHP
-
-
-require 'championlist.php';
-
-#print_r($champions);
+if (isset($_POST["commentName"]) && isset($_POST["comment"])) {
+		
 	
-
-require 'vendor/autoload.php';
-use LeagueWrap\Api;
-
-$api = new Api('3c89c9d9-6196-463b-9c7b-2ef2f83540f8');
-
-$summonerAPI = $api->summoner();
-$gameAPI = $api->game();
-
-$name = $_POST['name'];
-
-$summoner = $summonerAPI->info($name);
-
-$recentGames = $gameAPI->recent($summoner->id);
-
-echo "<img src='http://avatar.leagueoflegends.com/na/". ($name) . ".png' >" ;
-echo "<br>". ($name) ."'s Past 10 Games: <br> <pre>";
-
-function fixlolnum($lolnumber) {
-	if ($lolnumber == ''){
-	  return 0;
-	} else {
-	  return $lolnumber;
-	}
+$insert = mysqli_query($connection, "INSERT INTO `comments` (`name`,`words`) VALUES ('".sanitize($_POST['commentName'])."', '".sanitize($_POST['comment'])."')");
 }
 
-
-function fixlolname($lolname) {
-	$lolname = str_replace(" ", "", $lolname);
-	$lolname = str_replace("'", "", $lolname);
-	$lolname = str_replace(".", "", $lolname);
-	return $lolname;
-}
-
-?>
-<table>
-<tr>
-	<td></td>
-	<td>Stats</td>
-	<td></td>
-</tr>
-<?PHP
-
-foreach ($recentGames->games as $gameNum => $game) {
-	$championId = $game->championId;
-	$championName = $champions[$championId];	
-	echo "<!---#$championId--->\n";
-
-	echo "<tr>";
-	echo "<td>";
-	echo "<img src='/images/" . fixlolname($championName) . "Square.png'>";	
-	echo "<br>";
-	echo $championName;
-	echo "</td>";
-
-	echo "<td>";
-	echo fixlolnum($game->stats->championsKilled);
-	echo "/";
-	echo fixlolnum($game->stats->numDeaths);
-	echo "/";
-	echo fixlolnum($game->stats->assists);
-	echo "</td>";
-	
-
-	echo "<td>";
-	$won = $game->stats->win;
-
-	if ($won == 1){
-	  echo "<span class='big victory'>Victory</span>";
-	}
-	else {
-	  echo "<span class='big defeat'>Defeat</span>";
-
-	}
-	echo "<br>";
-	if (fixlolnum($game->stats->numDeaths) > fixlolnum($game->stats->championsKilled)+4) {
-	  echo " ...Goddamn feeder.";
-	}
-	if ((fixlolnum($game->stats->championsKilled) > fixlolnum($game->stats->numDeaths)+4) && $won == 1) {
-	  echo " ...Good job!";
-	}	
-
-	echo "</td>";
-	echo "</tr>\n\n";
-}
-#foreach ($champions as $championId => $championName) {
-#	
-#	echo "<img src='/images/" . fixlolname($championName) . "Square.png'>";	
-#}
-
-#print_r($recentGames->games[0]->stats);
-?>
-</table>
-<?PHP
 require 'comments.php';
 ?>
+<form action="index.php" method="post">
+        Name:
+	<br>
+        <input type="text" name="commentName">
+        <br>
+	Comment:
+	<br>
+	<input type="text" name="comment">
+	<br>
+        <input type="submit" value="submit">
+</form>
+
 </body>
 </html>
